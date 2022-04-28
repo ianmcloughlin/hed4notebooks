9fb06e4e22f1b5be4b5bc9d4a9a1d5d02a41772f
Ian McLoughlin <mcloughlin.ian@gmail.com>
2022-04-28 22:26:35 +0100

    Theme

---
+++ share/jupyter/labextensions/jupyter_theme_atu/labextension/static/lib_index_js.9e1b75d3d5308bdb2431.js
@@ -0,0 +1,41 @@
+"use strict";
+(self["webpackChunkjupyter_theme_atu"] = self["webpackChunkjupyter_theme_atu"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+
+/**
+ * Initialization data for the jupyter_theme_atu extension.
+ */
+const plugin = {
+    id: 'jupyter_theme_atu:plugin',
+    autoStart: true,
+    requires: [_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.IThemeManager],
+    activate: (app, manager) => {
+        console.log('JupyterLab extension jupyter_theme_atu is activated!');
+        const style = 'jupyter_theme_atu/index.css';
+        manager.register({
+            name: 'jupyter_theme_atu',
+            isLight: true,
+            load: () => manager.loadCSS(style),
+            unload: () => Promise.resolve(undefined)
+        });
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.9e1b75d3d5308bdb2431.js.map
